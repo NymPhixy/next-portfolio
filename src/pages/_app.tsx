40d29e484d6c0f648eab920b8b549db6bde82aab
NymPhixy <r.g.b.janssen@st.hanze.nl>
2025-03-02 20:28:19 +0100

    portfolio pages setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../app/layout";
-import "../styles/globals.css"; // Ensure you have Tailwind CSS setup
+import "@/app/globals.css"; // Ensure you have Tailwind CSS setup
 
 import type { AppProps } from "next/app";
 
