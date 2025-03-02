782380e369d87522520d44c61c70012a3e534874
Ruben Janssen <r.g.b.janssen@st.hanze.nl>
2025-03-02 21:06:45 +0100

    update
    latest commit my project

--- src/app/portfolio/[id]/page.tsx
+++ src/app/portfolio/[id]/page.tsx
@@ -4,11 +4,25 @@ import { useParams } from "next/navigation";
 import Link from "next/link";
 
 const portfolioItems: {
-  [key: string]: { title: string; description: string };
+  [key: string]: {
+    title: string;
+    description: string;
+    content?: { type: string; value: string }[];
+  };
 } = {
   item1: {
     title: "Project 1",
     description: "Details about Project 1...",
+    content: [
+      {
+        type: "text",
+        value: "We zijn dit project begonnen....",
+      },
+      {
+        type: "text",
+        value: "Daarna hebben we dit gedaan....",
+      },
+    ],
   },
   item2: {
     title: "Project 2",
@@ -30,6 +44,19 @@ function PortfolioItem() {
       <Link href="/portfolio">Back to Portfolio</Link>
       <h1>{item.title}</h1>
       <p>{item.description}</p>
+      <div className="flex flex-col gap-16">
+        {item.content &&
+          item.content.map((contentItem, index) => (
+            <div className="" key={index}>
+              {contentItem.type === "text" && (
+                <p className="text-leading">{contentItem.value}</p>
+              )}
+              {contentItem.type === "image" && (
+                <img src={contentItem.value} alt="" />
+              )}
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
